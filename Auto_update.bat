31d1ff294ab0767131716efca3bba6fdd981a11c
Carl Halvorson <38861925+chalvorson@users.noreply.github.com>
2020-10-02 20:06:56 -0600

    Script delay before sysmon update

--- Auto_update.bat
+++ Auto_update.bat
@@ -1,5 +1,6 @@
 @echo on
 cd C:\ProgramData\sysmon\
 @powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chalvorson/sysmon-config/master/sysmonconfig-export.xml" -OutFile "C:\ProgramData\sysmon\sysmonconfig-export.xml"
+timeout 30
 sysmon64 -c sysmonconfig-export.xml
 exit
