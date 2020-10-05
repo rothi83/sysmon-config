b4bbf3d9b47ef30316d6c52e4f3fd41a4031dbfa
Stefan Rothenbühler <stefan.rothenbuehler@infoguard.ch>
2020-10-05 13:43:39 +0200

    Clipboard

--- Auto_update.bat
+++ Auto_update.bat
@@ -1,6 +1,6 @@
 @echo on
-cd C:\ProgramData\sysmon\
-@powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chalvorson/sysmon-config/master/sysmonconfig-export.xml" -OutFile "C:\ProgramData\sysmon\sysmonconfig-export.xml"
+cd C:\Sysmon\
+@powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rothi83/sysmon-config/master/sysmonconfig-export.xml" -OutFile "C:\Sysmon\sysmonconfig-export.xml"
 timeout 30
 sysmon64 -c sysmonconfig-export.xml
 exit
--- install_sysmon.bat
+++ install_sysmon.bat
@@ -12,23 +12,23 @@ if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
 if "%hour:~1,1%"=="" set hour=0%hour%
 if "%minute:~1,1%"=="" set minute=0%minute%
 set tasktime=%hour%:%minute%
-mkdir C:\ProgramData\sysmon
-pushd "C:\ProgramData\sysmon\"
+mkdir C:\Sysmon
+pushd "C:\Sysmon\"
 echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\sysmon64.exe')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\Sysmon\sysmon64.exe')"
 echo [+] Downloading Sysmon config...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/chalvorson/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/chalvorson/sysmon-config/master/Auto_update.bat','C:\ProgramData\sysmon\Auto_Update.bat')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rothi83/sysmon-config/master/sysmonconfig-export.xml','C:\Sysmon\sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rothi83/sysmon-config/master/Auto_update.bat','C:\Sysmon\Auto_Update.bat')"
 sysmon64.exe -accepteula -i sysmonconfig-export.xml
 echo [+] Sysmon Successfully Installed!
-attrib +s +h +r c:\ProgramData\sysmon
-echo Y | cacls c:\ProgramData\Sysmon /e /p everyone:n
-echo Y | cacls c:\ProgramData\Sysmon /p system:f
-echo Y | cacls c:\ProgramData\Sysmon /p Administrators:f
+attrib +s +h +r c:\Sysmon
+echo Y | cacls c:\Sysmon /e /p everyone:n
+echo Y | cacls c:\Sysmon /p system:f
+echo Y | cacls c:\Sysmon /p Administrators:f
 sc failure Sysmon64 actions= restart/10000/restart/10000// reset= 120
 echo [+] Sysmon Directory Permissions Reset and Services Hidden
 sc sdset Sysmon64 D:(D;;DCLCWPDTSD;;;IU)(D;;DCLCWPDTSD;;;SU)(D;;DCLCWPDTSD;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)
 echo [+] Creating Auto Update Task set to Hourly..
-SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\ProgramData\sysmon\Auto_Update.bat /F /ST %tasktime%
+SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\Sysmon\Auto_Update.bat /F /ST %tasktime%
 timeout /t 10
 exit
